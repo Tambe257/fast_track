0a69d842d0101880a146676a1c2ce853f4e8c109
Tony Tambe <tambe257@gmail.com>
2014-08-18 13:41:03 -0400

    Added admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818173843) do
+ActiveRecord::Schema.define(version: 20140818174018) do
 
   create_table "scores", force: true do |t|
     t.boolean  "test_type",  limit: 255
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140818173843) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
