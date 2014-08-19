64dea723fe9a9bde59945810f8efbd8757164935
Tony Tambe <tambe257@gmail.com>
2014-08-19 13:25:11 -0400

    Updated Scores form and views

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,6 +1,6 @@
 class Score < ActiveRecord::Base
 
-	attr_accessible :name, :test_type, :month, :year, :math, :reading, :writing, :essay, :science, :english
+	attr_accessible :name, :test_type, :month, :year, :math, :reading, :writing, :essay, :science, :english, :comment, :day
 
 	belongs_to :user
 end
