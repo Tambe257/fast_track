3a83f54f4499334a3c4c2bc06f68121d79b9a6de
Tony Tambe <tambe257@gmail.com>
2014-08-26 13:46:30 -0400

    Updating the score show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   	if User.find(params[:id]) == current_user || current_user.admin?
   		@user = User.find(params[:id])
   		@scores = @user.scores
+  		@score_act = @scores.where(:name => 'ACT')
+  		@score_sat = @scores.where(:name => 'SAT')
+  		@score_psat = @scores.where(:name => 'PSAT')
   	else
   		redirect_to root_path
   	end	
