d41442d9db0d9c9f58cb2d8a6d3e7cdb8e0a3ff9
Tony Tambe <tambe257@gmail.com>
2014-09-15 13:52:41 -0400

    Temp Took Out the New Javascript

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,12 +2,3 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(function(){
-    $(".score_table").keyup(function() {
-      var textValue = $(this).val();
-      if(textValue === "Actual"){
-        // add css class or any manipulation to your dom.
-        $('#test_type').css('background-color','green');
-      }
-    });
-});
