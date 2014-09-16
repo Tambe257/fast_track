f6f6a7363c7029ca5ed96eb8911795c3c9a9ab62
Tony Tambe <tambe257@gmail.com>
2014-09-16 13:00:07 -0400

    Fixed comments in users.js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,14 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
 
+
+$(function(){
+    $("#scores tr").ready(function() {
+      var textValue = $(this).val();
+      if(textValue === 'Practice'){
+        // add css class or any manipulation to your dom.
+        $("#scores tr").addClass("score_table_color");
+      }
+    });
+});
