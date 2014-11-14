b6f48dafa60a9e1bfce72548ae88cb3f01f4d7b8
Tony Tambe <tambe257@gmail.com>
2014-11-14 13:36:52 -0500

    Added the pages_controller.rb back

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def students
+    if user_signed_in? && current_user.admin? 
+      @users = User.all
+    else
+      redirect_to root_path
+    end   
+  end  
+  
+  def admin
+    if user_signed_in? && current_user.admin?
+      @users = User.all
+    else
+      redirect_to root_path
+    end   
+  end
+
+  def guide
+    @chapters = Chapter.order('ch_order asc')
+  end  
+    
+end
