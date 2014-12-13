e9c8e013d667eeabc5a2045485794d024cb87591
Tony Tambe <tambe257@gmail.com>
2014-12-13 10:01:24 -0500

    Changes to the initializer assets

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,6 +1,6 @@
 ActionController::Base.asset_host = Proc.new { |source, request|
   if request.env["REQUEST_PATH"].include? ".pdf"
-    "file://#{Rails.root.join('public')}"
+    "file://#{Rails.root.join('app/assets/stylesheets/bootstrap_and_customization.css.scss')}"
   else
     "#{request.protocol}#{request.host_with_port}"
   end
